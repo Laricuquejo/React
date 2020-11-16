02e2d36317fbb716bc22971b45bba25986c310de
Laricuquejo <larissa@cuquejo.org>
2020-11-16 17:06:43 +0100

    ep 3

---
+++ demo-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { HelloWorld } from './Components/HelloWorld';
+
+ReactDOM.render(
+    React.createElement(HelloWorld),
+    document.querySelector('#root'),
+)
