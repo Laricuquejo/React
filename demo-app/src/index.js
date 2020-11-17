85d5b4c75b715448e63de7471a1aee138aceae7c
Laricuquejo <larissa@cuquejo.org>
2020-11-17 17:04:24 +0100

    ep 4

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { HelloWorld } from './Components/HelloWorld';
+import { CarTool } from './Components/CarTool';
+
+const carsList = [
+    { id: 1, make: 'Ford', model: 'Fusion Hybrid', year: 2018, color: 'silver', price: 30000 },
+    { id: 2, make: 'Tesla', model: 'S', year: 2017, color: 'red', price: 120000 },
+
+];
 
 ReactDOM.render(
-    React.createElement(HelloWorld),
+    <CarTool cars={carsList} />,
     document.querySelector('#root'),
 )
